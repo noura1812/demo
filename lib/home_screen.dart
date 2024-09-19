9c22c03b4f1e458720d3516274c982b67f36905c
noura <nouraabozad@gmail.com>
2024-09-19 20:47:21 +0300

    new edit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'package:demo/register.dart';
-
 class HomeScreen extends StatelessWidget {
   static const String routName = 'home_screen';
   String name;
   String email;
   HomeScreen({
-    Key? key,
+    super.key,
     required this.name,
     required this.email,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
